feb4e0f270e9ebee094e2ea10a098cb012a04215
telmoDev <telmodanielramirez@gmail.com>
2026-04-29 21:07:41 -0500

    Refactor astro.config.mjs for consistency and update output configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import alpinejs from '@astrojs/alpinejs';
-import node from '@astrojs/node';
+import alpinejs from "@astrojs/alpinejs";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  adapter: node({ mode: 'standalone' }),
+  output: "server",
+  adapter: node({ mode: "standalone" }),
   vite: {
-      plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [alpinejs()]
+  integrations: [alpinejs()],
 });
